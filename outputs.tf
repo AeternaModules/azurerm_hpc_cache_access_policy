7d07f7c4bac161b386aeb7c613d8021803193e34
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "hpc_cache_access_policies" {
-  description = "All hpc_cache_access_policy resources"
-  value       = azurerm_hpc_cache_access_policy.hpc_cache_access_policies
-}
 output "hpc_cache_access_policies_access_rule" {
-  description = "List of access_rule values across all hpc_cache_access_policies"
-  value       = [for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : v.access_rule]
+  description = "Map of access_rule values across all hpc_cache_access_policies, keyed the same as var.hpc_cache_access_policies"
+  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.access_rule }
 }
 output "hpc_cache_access_policies_hpc_cache_id" {
-  description = "List of hpc_cache_id values across all hpc_cache_access_policies"
-  value       = [for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : v.hpc_cache_id]
+  description = "Map of hpc_cache_id values across all hpc_cache_access_policies, keyed the same as var.hpc_cache_access_policies"
+  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.hpc_cache_id }
 }
 output "hpc_cache_access_policies_name" {
-  description = "List of name values across all hpc_cache_access_policies"
-  value       = [for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : v.name]
+  description = "Map of name values across all hpc_cache_access_policies, keyed the same as var.hpc_cache_access_policies"
+  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.name }
 }
 
