7921389e3cd922f978df03fd1ffff18394979d60
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "hpc_cache_access_policies_id" {
   description = "Map of id values across all hpc_cache_access_policies, keyed the same as var.hpc_cache_access_policies"
-  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.id }
+  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "hpc_cache_access_policies_access_rule" {
   description = "Map of access_rule values across all hpc_cache_access_policies, keyed the same as var.hpc_cache_access_policies"
-  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.access_rule }
+  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.access_rule if v.access_rule != null && length(v.access_rule) > 0 }
 }
 output "hpc_cache_access_policies_hpc_cache_id" {
   description = "Map of hpc_cache_id values across all hpc_cache_access_policies, keyed the same as var.hpc_cache_access_policies"
-  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.hpc_cache_id }
+  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.hpc_cache_id if v.hpc_cache_id != null && length(v.hpc_cache_id) > 0 }
 }
 output "hpc_cache_access_policies_name" {
   description = "Map of name values across all hpc_cache_access_policies, keyed the same as var.hpc_cache_access_policies"
-  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.name }
+  value       = { for k, v in azurerm_hpc_cache_access_policy.hpc_cache_access_policies : k => v.name if v.name != null && length(v.name) > 0 }
 }
 
